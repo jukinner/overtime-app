131b1d451c9d0e76d8aa3c67243f0c4864d8b87d
Jukinner <jukinner@gmail.com>
2018-03-06 15:46:08 -0700

    updated seeds and created tests for Audit Log

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,4 +20,8 @@ puts "1 Admin User created"
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
 
+100.times do |audit_log|
+  AuditLog.create!(user_id: @user.id, status: 0, start_date:(Date.today - 6.days))
+end
+
 puts "100 posts have been created"
--- spec/factories/audit_logs.rb
+++ spec/factories/audit_logs.rb
@@ -1,8 +1,10 @@
 FactoryBot.define do
   factory :audit_log do
-    user nil
+    user 
     status 1
-    start_date "2018-03-06"
-    end_date "2018-03-06"
+    start_date (Date.today - 6.days)
+    end_date nil
   end
 end
+
+    
