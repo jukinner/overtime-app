ae593c359401a1f2f036f0518e4e9c31cd386edc
Jukinner <jukinner@gmail.com>
2018-03-07 09:47:50 -0700

    Added pagination feature to Audit logs and Posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 
 puts "1 User created"
 
-AdminUser.create(email: "admin@test.com", 
+@admin = AdminUser.create(email: "admin@test.com", 
                 password: "asdfasdf", 
                 password_confirmation: "asdfasdf", 
                 first_name: "Admin", 
@@ -19,6 +19,10 @@ puts "1 Admin User created"
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
 end
+
+# 100.times do |post|
+#   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @admin.id, overtime_request: 2.5)
+# end
 
 100.times do |audit_log|
   AuditLog.create!(user_id: @user.id, status: 0, start_date:(Date.today - 6.days))
